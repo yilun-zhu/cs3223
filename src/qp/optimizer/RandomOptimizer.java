2cb51ff68a274651433961b8d4e0b76fc7becea5
Yilun Zhu <yilun95@gmail.com>
2019-03-17 11:35:05 +0800

    hashjoin init

--- src/qp/optimizer/RandomOptimizer.java
+++ src/qp/optimizer/RandomOptimizer.java
@@ -428,8 +428,10 @@ public class RandomOptimizer{
 
 	    case JoinType.HASHJOIN:
 
-		NestedJoin hj = new NestedJoin((Join) node);
-                /* + other code */
+		HashJoin hj = new HashJoin((Join) node);
+                hj.setLeft(left);
+                hj.setRight(right);
+                hj.setNumBuff(numbuff);
 		return hj;
 	    default:
 		return node;
